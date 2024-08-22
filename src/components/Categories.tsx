509d5c1dfc62f75a33ad333c084a8f8432bdcdb3
Shubham Sharma <shubhamsharma2061999@gmail.com>
2024-08-22 12:44:05 +0530

    build production

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,3 +1,30 @@
+// "use client"
+// import React, { useContext, useLayoutEffect } from "react";
+// import Categorycard from "./Categorycard";
+// import Categorycontext from "@/context/Categorycontext";
+
+// const Categories = ({ categories }: any) => {
+
+//   const {changeCategory} = useContext (Categorycontext);
+
+//   useLayoutEffect(() =>{
+//     changeCategory(categories?.data[0].attributes.Title)
+//   }, [])
+
+//   return (
+//     <div className="flex gap-6 mb-8 ">
+//       {categories?.data?.map((category: any) => (
+//         <div key={category.id}>
+//           <Categorycard cat={category} />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Categories;
+
+
 "use client"
 import React, { useContext, useLayoutEffect } from "react";
 import Categorycard from "./Categorycard";
@@ -5,11 +32,14 @@ import Categorycontext from "@/context/Categorycontext";
 
 const Categories = ({ categories }: any) => {
 
-  const {changeCategory} = useContext (Categorycontext);
+  const { changeCategory } = useContext(Categorycontext);
 
-  useLayoutEffect(() =>{
-    changeCategory(categories?.data[0].attributes.Title)
-  }, [])
+  useLayoutEffect(() => {
+    if (categories?.data?.length > 0) {
+      const title = categories.data[0].attributes.Title;
+      changeCategory(title);
+    }
+  }, [categories, changeCategory]); // Added categories and changeCategory to the dependency array
 
   return (
     <div className="flex gap-6 mb-8 ">
